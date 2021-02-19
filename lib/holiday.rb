fcc7116069d8035f850e59927279ef1c3b154066
Devorah Posner <dleahposner@gmail.com>
2021-02-19 01:26:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, season_details|
     season_details.each do |holidays, supplies|
-      if 
+      if supplies.include?("BBQ")
       binding.pry
   end
   
