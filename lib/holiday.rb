92e84df0a37a76e4f68d23ee628902776959fd94
Devorah Posner <dleahposner@gmail.com>
2021-02-19 01:05:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,8 +76,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  # holiday_hash.each do |
-    # binding.pry
+  holiday_hash.each do |
+    binding.pry
   
 
 end
